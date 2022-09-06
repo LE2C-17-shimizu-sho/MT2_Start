b6ffe353b26480c4f2da0754813e9d1f40924a6f
naoki147 <k021g1131@g.neec.ac.jp>
2022-09-06 12:32:02 +0900

    gamescene追加

---
+++ gamescene.cpp
@@ -0,0 +1,101 @@
+#include"gamescene.h"
+#include"DxLib.h"
+
+GameScene::~GameScene(){
+	delete	player;
+};
+
+void	GameScene::Initialize() {
+	player = new	Player;
+	player->Initialize();
+}
+
+void	GameScene::Update() {
+	
+	// 最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
+	for (int i = 0; i < 256; ++i)
+	{
+		oldkeys[i] = keys[i];
+	}
+	// 最新のキーボード情報を取得
+	GetHitKeyStateAll(keys);
+	//---------  ここからにプログラムを記述  ---------//
+	switch (scene)
+	{
+		// タイトル
+	case 0:
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
+			scene = 1;
+			player->State();
+		}
+		break;
+
+		// 操作説明
+	case 1:
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
+			scene = 2;
+		}
+		break;
+
+		// ゲーム
+	case 2:
+		player->Update(keys, oldkeys);
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
+			scene = 3;
+		}
+		break;
+
+		// リザルト(クリア)
+	case 3:
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
+			scene = 0;
+		}
+		break;
+
+		// リザルト(ゲームオーバー)
+	case 4:
+		if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+		{
+			scene = 0;
+		}
+		break;
+	}
+}
+
+void	GameScene::Draw() {
+	int	color = GetColor(255, 255, 255);
+	switch (scene)
+	{
+		// タイトル
+	case 0:
+		DrawFormatString(0, 0, color, "タイトル");
+		DrawCircle(370, 640, 20, GetColor(255, 255, 255), true);
+		DrawCircle(370, 640, 120, GetColor(255, 255, 255), false);
+		break;
+
+		// 操作説明
+	case 1:
+		DrawFormatString(0, 0, color, " 操作説明");
+		break;
+
+		// ゲーム
+	case 2:
+		player->Draw();
+		DrawFormatString(0, 0, color, "ゲーム");
+		break;
+
+		// リザルト(クリア)
+	case 3:
+		DrawFormatString(0, 0, color, "クリア");
+		break;
+
+		// リザルト(ゲームオーバー)
+	case 4:
+		DrawFormatString(0, 0, color, "オーバー");
+		break;
+	}
+}
---
+++ gamescene.h
@@ -0,0 +1,29 @@
+#pragma once
+#include"player.h"
+
+class GameScene
+{
+public:
+	//GameScene();
+	~GameScene();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void	Initialize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void	Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void	Draw();
+private:
+	// 最新のキーボード情報用
+	char keys[256] = { 0 };
+
+	// 1ループ(フレーム)前のキーボード情報
+	char oldkeys[256] = { 0 };
+	int scene = 0;
+	Player* player;
+};
