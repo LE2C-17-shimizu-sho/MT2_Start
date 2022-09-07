f097fb08567efe2edbad92576661716662d02678
oumi0310 <k021g1382@g.neec.ac.jp>
2022-09-07 15:57:36 +0900

    Map.hとMap.cppを書いた

--- Map.cpp
+++ Map.cpp
@@ -1,1 +1,27 @@
-#include"Map.h"
+#include "Map.h"
+
+void Map::Initialize()
+{
+	// ‰æ‘œ‚ğ‰Šú‰»
+	block = LoadGraph("block.png");
+}
+
+void Map::Update()
+{
+
+}
+
+void Map::Draw()
+{
+	// •`‰æˆ—
+	for (int y = 0; y < 45; y++)
+	{
+		for (int x = 0; x < 80; x++)
+		{
+			if (map[y][x] == 1)
+			{
+				DrawGraph(x * BLOCK_SIZE, y * BLOCK_SIZE, block, true);
+			}
+		}
+	}
+}
