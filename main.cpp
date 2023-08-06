6dcc921f97b4b78dbd521eb199617ca8f53ed4b8
simizu <94036084+simizuzu@users.noreply.github.com>
2023-08-07 01:19:12 +0900

    テスト開始

--- main.cpp
+++ main.cpp
@@ -1,133 +1,98 @@
 #include "DxLib.h"
-#include "Vector3.h"
-//球の描画
-int DrawSphere3D(const Vector3& CenterPos, const float r, const int DivNum,
-	const unsigned int DifColor, const unsigned int SpcColor, const int FillFlag);
+#include <vector>
 
-int DrawLine3D(const Vector3& Pos1, const Vector3& Pos2, const unsigned int Color);
-
-
-// ウィンドウのタイトルに表示する文字列
-const char TITLE[] = "LE2C_17_シミズショウ: タイトル";
-
-// ウィンドウ横幅
-const int WIN_WIDTH = 1024;
-
-// ウィンドウ縦幅
-const int WIN_HEIGHT = 576;
-
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
-                   _In_ int nCmdShow) {
-	// ウィンドウモードに設定
-	ChangeWindowMode(TRUE);
-
-	// ウィンドウサイズを手動では変更させず、
-	// かつウィンドウサイズに合わせて拡大できないようにする
-	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
-
-	// タイトルを変更
-	SetMainWindowText(TITLE);
-
-	// 画面サイズの最大サイズ、カラービット数を設定(モニターの解像度に合わせる)
-	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);
-
-	// 画面サイズを設定(解像度との比率で設定)
-	SetWindowSizeExtendRate(1.0);
-
-	// 画面の背景色を設定する
-	SetBackgroundColor(0x00, 0x00, 0x00);
-
-	// DXlibの初期化
-	if (DxLib_Init() == -1) { return -1; }
-
-	// (ダブルバッファ)描画先グラフィック領域は裏面を指定
-	SetDrawScreen(DX_SCREEN_BACK);
-
-	// Ｚバッファを有効にする
-	SetUseZBuffer3D(TRUE);
-
-	// Ｚバッファへの書き込みを有効にする
-	SetWriteZBuffer3D(TRUE);
-
-	//クリップ面      近     遠
-	SetCameraNearFar(1.0f, 1000.0f);//カメラの有効範囲の設定
-	SetCameraScreenCenter(WIN_WIDTH / 2.0f, WIN_HEIGHT / 2.0f);//画面の中心をカメラの中心に合わせる
-
-	SetCameraPositionAndTargetAndUpVec(
-		VGet(0.0f, 0.0f, -100.0f),//カメラの位置
-		VGet(0.0f, 0.0f, 0.0f),//カメラの注視点
-		VGet(0.0f, 1.0f, 0.0f));//カメラの上の向き
-
-	// 画像などのリソースデータの変数宣言と読み込み
-
-	Vector3 position(0, 0, 0);//位置
-	Vector3 velocity(0.0f, 0.0f, 0.5f);//速度
-
-	// ゲームループで使う変数の宣言
-
-
-	// 最新のキーボード情報用
-	char keys[256] = {0};
-
-	// 1ループ(フレーム)前のキーボード情報
-	char oldkeys[256] = {0};
-
-	// ゲームループ
-	while (true) {
-		// 最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
-		// 最新のキーボード情報を取得
-		GetHitKeyStateAll(keys);
-
-		// 画面クリア
-		ClearDrawScreen();
-		//---------  ここからプログラムを記述  ----------//
-
-		// 更新処理
-		position += velocity;
-
-		// 描画処理
-		ClearDrawScreen();
-		DrawSphere3D(position, 80.0f, 32, GetColor(255, 0, 0), GetColor(255, 255, 255), true);
-
-		//---------  ここまでにプログラムを記述  ---------//
-		// (ダブルバッファ)裏面
-		ScreenFlip();
-
-		// 20ミリ秒待機(疑似60FPS)
-		WaitTimer(20);
-
-		// Windowsシステムからくる情報を処理する
-		if (ProcessMessage() == -1) {
-			break;
-		}
-
-		// ESCキーが押されたらループから抜ける
-		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) {
-			break;
-		}
-	}
-	// Dxライブラリ終了処理
-	DxLib_End();
-
-	// 正常終了
-	return 0;
+// ベジェ基底関数を計算する関数
+double bezier_basis(int i, int n, double t) {
+    double coeff = 1.0;
+    for (int j = 0; j < i; ++j) {
+        coeff *= (1 - t); // (1 - t)のi乗を計算
+    }
+    for (int j = i + 1; j <= n; ++j) {
+        coeff *= t; // tのn-i乗を計算
+    }
+    return coeff;
 }
 
-//オーバーロード
-//球の描画
-int DrawSphere3D(const Vector3& CenterPos, const float r, const int DivNum,
-	const unsigned int DifColor, const unsigned int SpcColor, const int FillFlag)
-{
-	VECTOR centerPos = { CenterPos.x,CenterPos.y,CenterPos.z };
+// 有理ベジェ曲線を計算する関数
+std::vector<double> rational_bezier_curve(std::vector<std::vector<double>>& control_points, std::vector<double>& weights, double t) {
+    int n = control_points.size() - 1;
+    std::vector<double> result(3, 0.0); // 3次元の場合
+    double sum_basis_times_weights = 0.0;
+    double sum_basis_times_weights_times_points = 0.0;
 
-	return DrawSphere3D(centerPos, r, DivNum, DifColor, SpcColor, FillFlag);
+    // ベジェ基底関数の計算と重み付きの合計を求める
+    for (int i = 0; i <= n; ++i) {
+        double basis = bezier_basis(i, n, t);
+        double weighted_basis = basis * weights[i];
+        sum_basis_times_weights += weighted_basis;
+
+        for (int j = 0; j < 3; ++j) { // 3次元の場合
+            sum_basis_times_weights_times_points += weighted_basis * control_points[i][j];
+        }
+    }
+
+    // 有理ベジェ曲線上の点を計算
+    for (int i = 0; i < 3; ++i) { // 3次元の場合
+        result[i] = sum_basis_times_weights_times_points / sum_basis_times_weights;
+    }
+
+    return result;
 }
 
-//線分の描画
-int DrawLine3D(const Vector3& Pos1, const Vector3& Pos2, const unsigned int Color)
-{
-	VECTOR p1 = { Pos1.x,Pos1.y,Pos1.z };
-	VECTOR p2 = { Pos2.x,Pos2.y,Pos2.z };
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    // DxLibの初期化
+    if (DxLib_Init() == -1) {
+        return -1;
+    }
 
-	return DrawLine3D(p1, p2, Color);
+    // ウィンドウを作成
+    ChangeWindowMode(TRUE);
+    SetGraphMode(800, 600, 32);
+    if (SetMainWindowText("Rational Bezier Curve") == -1) {
+        return -1;
+    }
+    if (DxLib_Init() == -1) {
+        return -1;
+    }
+
+    // 制御点とウェイトの定義
+    std::vector<std::vector<double>> control_points = {
+        {50.0, 200.0, 0.0},
+        {300.0, 100.0, 0.0},
+        {500.0, 300.0, 0.0},
+        {750.0, 100.0, 0.0},
+        {700.0, 400.0, 0.0}
+    };
+    std::vector<double> weights = { 1.0, 0.5, 2.0, 1.5, 1.0 };
+
+    // パラメータtの範囲（0.0～1.0）とステップ数
+    double t_min = 0.0;
+    double t_max = 1.0;
+    int steps = 100;
+
+    // メインループ
+    while (!ProcessMessage() && !ClearDrawScreen())
+    {
+
+        // 有理ベジェ曲線を描画
+        for (int i = 0; i <= steps; ++i) {
+            double t = t_min + (t_max - t_min) * i / steps;
+            std::vector<double> result = rational_bezier_curve(control_points, weights, t);
+            int x = static_cast<int>(result[0]);
+            int y = static_cast<int>(result[1]);
+            DrawPixel(x, y, GetColor(255, 255, 255));
+        }
+
+        // 画面の更新
+        ScreenFlip();
+
+        // キーが押されるまで待機
+        WaitKey();
+    }
+
+
+    // DxLibの終了処理
+    DxLib_End();
+
+    return 0;
 }
